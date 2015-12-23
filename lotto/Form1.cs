682d4c4936a376678f2456e35a37520dd8e57736
jack <jack.yang@hg-asia.com>
2015-12-23 17:21:43 +0800

    aa

--- lotto/Form1.cs
+++ lotto/Form1.cs
@@ -1,17 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
 using System.Windows.Forms.DataVisualization.Charting;
-using RDotNet;
 using System.Net;
-using System.IO;
 using System.Web.Script.Serialization;
 
 namespace lotto
